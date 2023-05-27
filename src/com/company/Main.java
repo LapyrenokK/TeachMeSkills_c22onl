b022ffceeff5687641cd79b616c4a85f760d8bbc
Kirill Lapyrenok <lapyronak.dev@gmail.com>
2023-05-27 11:11:02 +0200

    fixed bugs in homework3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,73 +1,231 @@
 package com.company;
 
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-//      Exercise 1
-        printPersonInf();
+        System.out.println("Exercise 1.1");
+        System.out.println(getMonthWithSwitchCase());
         System.out.println();
-//      Exercise 2
-        changeCharToNumber();
+
+        System.out.println("Exercise 1.2");
+        System.out.println(getMonthWithIfElse());
         System.out.println();
-//      Exercise 3
-        printBMI();
+
+        System.out.println("Exercise 1.3");
+        System.out.println(getEvenOddResult());
+        System.out.println();
+
+        System.out.println("Exercise 1.4");
+        System.out.println(getWeather());
+        System.out.println();
+
+        System.out.println("Exercise 1.5");
+        System.out.println(getColorOfTheReinbow());
+        System.out.println();
+
+        System.out.println("Exercise 2.1");
+        printOddNuumbers();
+        System.out.println();
+
+        System.out.println("Exercise 2.2");
+        printReverseNumbers();
+        System.out.println();
+
+        System.out.println("Exercise 2.3");
+        getResultSum();
+        System.out.println();
+
+        System.out.println("Exercise 2.4");
+        printNumbers7_98();
+        System.out.println();
+
+        System.out.println("Exercise 2.5");
+        printFirstNumbers10();
+        System.out.println();
+
+        System.out.println("Exercise 2.6");
+        printSquareNumbers();
+        System.out.println();
+
+        System.out.println("Exercise 3.1");
+        printFibNums();
+        System.out.println();
+
+        System.out.println("Exercise 3.2");
+        printDepositBank();
+        System.out.println();
+
+        System.out.println("Exercise 3.3");
+        printMultiplicationTable();
+
 
     }
-    static void printPersonInf()
-    {
-        System.out.print("Write your Name: ");
-        String name = cout().nextLine();
-        System.out.print("Write your Surname: ");
-        String surname = cout().nextLine();
-        System.out.print("Write your age: ");
-        short age = cout().nextShort();
-        System.out.print("Write your height: ");
-        int height = cout().nextInt();
-        System.out.print("Write your weight: ");
-        double weight = cout().nextDouble();
-        System.out.println("\n You preson data: \n Your Name - "+name+" \n Your Surname - "+surname+" \n You Age - "+age+" \n You Height - "+height+" \n You Weight - "+weight);
-    }
 
-
-    static void changeCharToNumber()
-    {
-        System.out.print("Input symbol: ");
-        char symbol = cout().next().charAt(0);
-        System.out.println("Symbol " + symbol + "=" +(int)symbol);
-    }
-
-    static void printBMI()
-    {
-        System.out.print("Enter your height : ");
-        double height = cout().nextDouble()/100;
-        System.out.print("Enter your weight : ");
-        double weigth = cout().nextDouble();
-        double heightM2 = Math.pow(height, 2);
-        double bmi = weigth / heightM2;
-        String formattedBMI = String.format("%.2f", bmi);
-        System.out.println("You BMI = " + formattedBMI);
-        if (bmi < 16) {
-            System.out.println("Classification - Severe Thinness");
-        } else if (bmi > 16 && bmi < 17) {
-            System.out.println("Classification - Moderate Thinness");
-        } else if (bmi > 17 && bmi < 18.5) {
-            System.out.println("Classification - Mild Thinness");
-        } else if (bmi > 18.5 && bmi < 25) {
-            System.out.println("Classification - Normal");
-        } else if (bmi > 25 && bmi < 30) {
-            System.out.println("Classification - Overweight");
-        } else if (bmi > 30 && bmi < 35) {
-            System.out.println("Classification - Obese Class I");
-        } else if (bmi > 35 && bmi < 40) {
-            System.out.println("Classification - Obese Class II");
-        } else {
-            System.out.println("Classification - Obese Class III");
+    static String getMonthWithSwitchCase() {
+        System.out.print("Method switch-case.\n Enter season number: ");
+        switch (cout().nextInt()) {
+            case 1, 2, 12:
+                return "Winter!";
+            case 3, 4, 5:
+                return "Spring!";
+            case 6, 7, 8:
+                return "Summer!";
+            case 9, 10, 11:
+                return "Autumn!";
+            default:
+                return "Тo such month!!!";
         }
     }
-    static Scanner cout (){
+
+    static String getMonthWithIfElse() {
+        System.out.print("Method if-else-if: \n Enter season number: ");
+        int season = cout().nextInt();
+        if (season == 1 || season == 2 || season == 12) {
+            return "Winter!";
+        } else if (season == 3 || season == 4 || season == 5) {
+            return "Spring!";
+        } else if (season == 6 || season == 7 || season == 8) {
+            return "Summer!";
+        } else if (season == 9 || season == 10 || season == 11) {
+            return "Autumn!";
+        } else {
+            return "Тo such month!!!";
+        }
+    }
+
+    static String getEvenOddResult() {
+        System.out.print("Enter your number: ");
+        int number = cout().nextInt();
+        if (number % 2 == 1) {
+            return "Number: " + number + " - Odd.";
+        } else {
+            return "Number: " + number + " - Even.";
+        }
+    }
+
+    static String getWeather() {
+        System.out.print("Enter temperature: ");
+        int temperature = cout().nextInt();
+        if (temperature > -5) {
+            return "Тепло";
+        } else if (-5 >= temperature && temperature > -20) {
+            return "Нормально";
+        } else if (-20 >= temperature) {
+            return "Холодно";
+        } else {
+            return null;
+        }
+    }
+
+    static String getColorOfTheReinbow() {
+        System.out.print("Enter number rainbow: ");
+        switch (cout().nextInt()) {
+            case 1:
+                return "RED";
+            case 2:
+                return "ORANGE";
+            case 3:
+                return "YELLOW";
+            case 4:
+                return "GREEN";
+            case 5:
+                return "CYAN";
+            case 6:
+                return "BLUE";
+            case 7:
+                return "VIOLET";
+            default:
+                return "It's not the color of the rainbow...";
+        }
+    }
+
+    static void printOddNuumbers() {
+        for (int i = 1; i <= 99; i++) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+
+    static void printReverseNumbers() {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i--;
+        } while (i >= 1);
+    }
+
+    static void getResultSum() {
+        System.out.print("Enter your number: ");
+        int number = cout().nextInt();
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += i;
+        }
+        System.out.println(count);
+    }
+
+    static void printNumbers7_98() {
+        int i = 0;
+        while (i < 98) {
+            i += 7;
+            System.out.print(i + " ");
+        }
+    }
+
+    static void printFirstNumbers10() {
+        for (int i = 0; i >= (-5 * 9); i -= 5) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void printSquareNumbers() {
+        int num = 10;
+        for (int i = 10; i <= 20; i++) {
+            System.out.println(i + "^2= " + i * i);
+        }
+    }
+
+    static void printFibNums() {
+
+        int sum = 1, fib1 = 1, fib2 = 1, i = 0;
+        for (int n = 0; n < 11; n++) {
+            while (i <= n - 2) {
+                sum = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = sum;
+                i++;
+            }
+            System.out.println(sum + " ");
+        }
+    }
+
+    static void printDepositBank() {
+        System.out.print("Enter your deposit amount in $: ");
+        float money = cout().nextFloat();
+        System.out.print("Enter number of months for deposit: ");
+        int months = cout().nextInt();
+        float sum = money, procent = 7 / 100f;
+        for (int i = 1; i <= months; i++) {
+            sum = sum * procent + sum;
+        }
+        String formatted = String.format("%.2f", sum);
+        System.out.println("Final amount = " + formatted + "$");
+    }
+
+    static void printMultiplicationTable() {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println("["+i + "*" + j + " = " + i * j+"]");
+            }
+            System.out.println();
+        }
+    }
+
+
+    static Scanner cout() {
         return new Scanner(System.in);
     }
 }
